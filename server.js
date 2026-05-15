d6bc41f7a2aff3866843ecf27c3010f981aae528
trabelsirahma95-maker <trabelsirahma95@gmail.com>
2026-05-15 18:50:02 +0100

    Update server.js

--- server.js
+++ server.js
@@ -20,9 +20,17 @@ app.get("/webhook", (req, res) => {
   const token = req.query["hub.verify_token"];
   const challenge = req.query["hub.challenge"];
 
-  if (mode && token === VERIFY_TOKEN) {
+  if (
+    mode === "subscribe" &&
+    token === VERIFY_TOKEN
+  ) {
+    console.log("WEBHOOK VERIFIED");
+
     return res.status(200).send(challenge);
+  } else {
+    return res.sendStatus(403);
   }
+});
 
   res.sendStatus(403);
 });
